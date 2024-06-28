08aa1bb21fac998baf94e7b03b8779a5f331bea5
Justin RIbarich <jribaric@nyit.edu>
2024-06-28 15:46:12 -0400

    yeah again

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Welcome to Chelsea and Colas website for there business.
+          Welcome to Chelsea and Colas website for their business.
         </p>
       <small style={{ fontSize: '14px' }} >Property of Justin Ribarich ©️</small>
       </header>
