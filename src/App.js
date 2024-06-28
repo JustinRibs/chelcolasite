81951658b4a0bb0e1ac1c7bd8ff1e52687f12cd2
Justin RIbarich <jribaric@nyit.edu>
2024-06-28 11:37:50 -0400

    yeah

--- src/App.js
+++ src/App.js
@@ -7,16 +7,9 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Welcome to Chelsea and Colas website for there business.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <small style={{ fontSize: '14px' }} >Property of Justin Ribarich ©️</small>
       </header>
     </div>
   );
